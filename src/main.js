709f20fcd95ab4dc3d847412da2ee083398e5637
razakhalid <ra97za@gmail.com>
2022-04-24 21:11:08 -0500

    set up router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router';
+import 'bootstrap';
+import jQuery from 'jquery';
+import 'popper.js';
+import '@/assets/app.scss';
 
-createApp(App).mount('#app')
+// Make jQuery available everywhere
+window.$ = window.jQuery = jQuery;
+
+let app;
+
+app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
